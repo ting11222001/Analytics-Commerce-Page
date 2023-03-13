4f8b417799625fd70744f42f2a67b7e5bb57c953
tiffany <ting11222001@gmail.com>
2023-03-13 20:55:16 +1030

    feat: 🎸 create navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <h1 className='text-red-500'>Hello</h1>
+    <div>
+      <Navbar />
+    </div>
   )
 }
