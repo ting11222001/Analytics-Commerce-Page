2d34bb6cb56294c54bfabbe09e3c0df186ccd061
tiffany <ting11222001@gmail.com>
2023-03-13 21:56:04 +1030

    feat: 🎸 add transition on the navbar opening

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+  const [navbar, setNavbar] = useState(false);
+
+  const handleNavbar = () => {
+    setNavbar(!navbar);
+  }
+
   return (
     <div className='text-white flex justify-between items-center max-w-[1024px] mx-auto h-24 px-4'>
       <h1 className='w-full text-3xl font-bold text-blue-500'>DATA.</h1>
@@ -16,13 +22,19 @@ const Navbar = (props: Props) => {
       </div>
 
       {/* Hamburger */}
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={handleNavbar}>
+        {navbar ? (
+          <AiOutlineClose size={20} />
+        ) : (
+          <AiOutlineMenu size={20} />
+        )}
+
       </div>
 
       {/* Mobile menu */}
-      <div className='fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-900 pt-24'>
-        <div className='flex flex-col bg-black p-4'>
+      <div className={`${navbar ? 'w-[60%]' : 'fixed left-[-100%]'} fixed top-0 left-0 h-full border-r  bg-black border-r-gray-900 pt-24 transition-all duration-500`}>
+        <h1 className='w-full text-3xl font-bold text-blue-500 p-4'>DATA.</h1>
+        <div className='flex flex-col p-4'>
           <a href="" className='p-4 uppercase border-b border-r-gray-600'>Home</a>
           <a href="" className='p-4 uppercase border-b border-r-gray-600'>Company</a>
           <a href="" className='p-4 uppercase border-b border-r-gray-600'>Resources</a>
@@ -31,7 +43,7 @@ const Navbar = (props: Props) => {
         </div>
       </div>
 
-    </div>
+    </div >
   )
 }
 
