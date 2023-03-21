095446fec9c8243e72899e6a5702905c738aa41c
tiffany <ting11222001@gmail.com>
2023-03-21 15:46:26 +1030

    feat: 🎸 create footer layout

---
+++ components/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare,
+} from 'react-icons/fa';
+
+
+type Props = {}
+
+const Footer = (props: Props) => {
+    return (
+        <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+            <div>
+                <h1 className='w-full text-3xl font-bold text-blue-500'>DATA.</h1>
+                <p className='py-4'>Monitor and gain insights from your data.</p>
+                <div className='flex md:w-[75%] my-6 justify-between'>
+                    <FaFacebookSquare size={30} />
+                    <FaInstagram size={30} />
+                    <FaTwitterSquare size={30} />
+                    <FaGithubSquare size={30} />
+                    <FaDribbbleSquare size={30} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Analytics from "@/components/Analytics";
 import Newsletters from "@/components/Newsletters";
 import Cards from "@/components/Cards";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Analytics />
       <Newsletters />
       <Cards />
+      <Footer />
     </div>
   )
 }
