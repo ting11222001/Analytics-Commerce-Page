bd66b5b8b31cecd79f20b0501c277095b79bccfc
tiffany <ting11222001@gmail.com>
2023-03-21 12:18:45 +1030

    feat: 🎸 create type animation and the rest of the hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Typed from 'react-typed';
 
 type Props = {}
 
@@ -6,11 +7,19 @@ const Hero = (props: Props) => {
     return (
         <div className='text-white'>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <p className='text-blue-500 font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
-                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
-                <div>
-                    <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible service for</p>
+                <p className='text-blue-500 font-bold'>GROWING WITH DATA ANALYTICS</p>
+                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold py-2'>Grow with data.</h1>
+                <div className='flex justify-center items-center'>
+                    <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible service for</p>
+                    <Typed className='md:text-5xl sm:text-4xl text-xl font-bold pl-2'
+                        strings={['B2B', 'SaaS', 'B2C']}
+                        typeSpeed={200}
+                        backSpeed={140}
+                        loop
+                    />
                 </div>
+                <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data to increase your revenue.</p>
+                <button className='bg-blue-500 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
             </div>
         </div>
     )
